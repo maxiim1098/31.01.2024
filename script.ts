bdbacbeb3e56065251a8e99f5377d31b71269441
maxiim1098 <156887699+maxiim1098@users.noreply.github.com>
2024-01-31 17:45:33 +1000

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,22 @@
+// class ClassName{}
+class Point{
+ x: number
+ y: number
+}
+let a: Point = new Point()
+function func(){
+    a.x = 1
+    a.y = 2
+}
+// console.log(a)
+// console.log(func)
+
+class Point2{
+    width: number
+    height: number
+}
+let b: Point2 = new Point2()
+function func2(){
+    b.width = 1
+    b.height = 2
+}
